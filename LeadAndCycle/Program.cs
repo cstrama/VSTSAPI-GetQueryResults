618110b27fde1ec32029157f7cc517cc956271e2
Scott <sg11769@gmail.com>
2016-05-30 10:23:48 -0400

    added JSON as classes

--- LeadAndCycle/Program.cs
+++ LeadAndCycle/Program.cs
@@ -12,6 +12,28 @@ using System.Xml;
 
 namespace CodeEvaler
 {
+    public class Rootobject
+    {
+        public string queryType { get; set; }
+        public string queryResultType { get; set; }
+        public DateTime asOf { get; set; }
+        public Column[] columns { get; set; }
+        public Workitem[] workItems { get; set; }
+    }
+
+    public class Column
+    {
+        public string referenceName { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Workitem
+    {
+        public int id { get; set; }
+        public string url { get; set; }
+    }
+
     public static class Program
     {
         public static void Main()
